c4df447254c0ff9acec7482ad6ebf5f7864af576
Гондарев Виктор <v.hondarev@gmail.com>
2022-08-11 00:03:59 +0300

    version 0.3.0

--- README.md
+++ README.md
@@ -1,19 +1,30 @@
-# Space Exploration Alkharid's Experience Mod Pack
+# Space Exploration Alkharid Experience Mod Pack
 
-# FAQ
+## Modpack Features
+- Cosmos gameplay
+- Advanced transport logistic
+- Advanced train and rocket constructions
+- Advanced military
+- RTS warfare
+- Harder aliens
 
 ### For whom this modpack
 If you have finished vanilla campaign and want to explore more with Cosmos and Aliens war
+
+### Why all downloadable dependencies is required
+This modpack aims to give full setup for new players that want to try different gameplay after finishing vanilla factorio but don't know where to start.
+Mod includes game changing and utilities dependencies that will help in planning base.
+Also this mod include some adaptation tweaks to mods to make them more logical and not overpowered for early base (see changelog).
 
 ### What included in this modpack
 - [Earendel mods](https://mods.factorio.com/user/Earendel) - Space Exploration/Bioms/AAI
 - [Rocket-Silo Construction](https://mods.factorio.com/mod/Rocket-Silo-Construction)
 - [Klonan mods](https://mods.factorio.com/user/Klonan) - Transport/Kombat/Mining/Construction Drones, Teleport and other...
 - [Veden mods](https://mods.factorio.com/user/Veden) - Base, Arsenal and Evolution
-- [Armoured Biters](https://mods.factorio.com/mod/ArmouredBiters)
-- [Arachnids](https://mods.factorio.com/mod/Arachnids)
-- [Cargo Ships](https://mods.factorio.com/mod/cargo-ships)
-- [Hovercraft](https://mods.factorio.com/mod/Hovercrafts)
+- [Armoured Biters](https://mods.factorio.com/mod/ArmouredBiters) - new biter type
+- [Arachnids](https://mods.factorio.com/mod/Arachnids) - new sprites for aliens
+- [Cargo Ships](https://mods.factorio.com/mod/cargo-ships) - water transport
+- [Hovercraft](https://mods.factorio.com/mod/Hovercrafts) - universal land transport
 - [Aircraft](https://mods.factorio.com/mod/Aircraft) and supporting mods (Aircraft Realism, Better Cargo Planes)
 - [Train Construction](https://mods.factorio.com/mod/trainConstructionSite)
 - [Night Brightness](https://mods.factorio.com/mod/NightBrightness) and other mods to support darker nights (RealisticFlashlight, DeadlockLargerLamp, Concrete_Lamppost_v11)
--- data.lua
+++ data.lua
@@ -3,28 +3,90 @@ require('__stdlib__/stdlib/data/data').Util.create_data_globals()
 
 -- Update Kombat Drones
 if mods["Kombat_Drones"] then
-  TECHNOLOGY("infantry-depot"):add_pack("logistic-science-pack")
+  TECHNOLOGY("infantry-depot")
+    :add_pack("logistic-science-pack")
+    :add_pack("military-science-pack")
+    :remove_prereq("military")
+    :add_prereq("logistics-2")
+    :add_prereq("fast-inserter")
+    :add_prereq("advanced-electronics")
+    :add_prereq("military-science-pack")
+
+  TECHNOLOGY("heavy-infantry")
+    :add_pack("military-science-pack")
+    :add_prereq("heavy-armor")
+
+  TECHNOLOGY("power-infantry")
+    :add_pack("chemical-science-pack")
+    :add_prereq("chemical-science-pack")
+    :add_prereq("modular-armor")
 
   RECIPE("infantry-depot")
-    :add_ingredient({type = "item", name = "electronic-circuit", amount = 10})
-    :add_ingredient({type = "item", name = "transport-belt", amount = 10})
-    :add_ingredient({type = "item", name = "inserter", amount = 10})
+    :add_ingredient({type = "item", name = "advanced-circuit", amount = 10})
+    :add_ingredient({type = "item", name = "fast-transport-belt", amount = 10})
+    :add_ingredient({type = "item", name = "fast-inserter", amount = 10})
 
   RECIPE("basic-infantry"):add_ingredient({type = "item", name = "electronic-circuit", amount = 2})
-  
   RECIPE("heavy-infantry"):add_ingredient({type = "item", name = "advanced-circuit", amount = 2})
   RECIPE("power-infantry"):add_ingredient({type = "item", name = "processing-unit", amount = 2})
 end
 
 -- Update Mining Drones
 if mods["Mining_Drones"] then
-  RECIPE("mining-depot"):add_ingredient({type = "item", name = "electronic-circuit", amount = 10})
-  RECIPE("mining-drone"):add_ingredient({type = "item", name = "electric-motor", amount = 1})
+  TECHNOLOGY {
+    type = "technology",
+    name = "mining-depot",
+    localised_name = {"mining-depot"},
+    icon = "__Mining_Drones__/data/technologies/mining_drones_tech.png",
+    icon_size = 256,
+    order = "a",
+    upgrade = false,
+    prerequisites = {"advanced-electronics", "mining-productivity-1"},
+    effects = {
+      {
+        type = "unlock-recipe",
+        recipe = "mining-depot"
+      },
+      {
+        type = "unlock-recipe",
+        recipe = "mining-drone"
+      },
+    },
+    unit = {
+      count = 200,
+      ingredients = {
+        {"automation-science-pack", 1},
+        {"logistic-science-pack", 1},
+      },
+      time = 45
+    }
+  }
+
+  TECHNOLOGY("mining-drone-mining-speed-1")
+    :add_prereq("mining-depot")
+  TECHNOLOGY("mining-drone-productivity-1")
+    :add_prereq("mining-depot")
+
+  RECIPE("mining-depot")
+    :set_enabled(false)
+    :add_ingredient({type = "item", name = "electronic-circuit", amount = 10})
+
+  RECIPE("mining-drone")
+    :set_enabled(false)
+    :add_ingredient({type = "item", name = "electric-motor", amount = 1})
 end
 
 
 -- Update Transport Drones
 if mods["Transport_Drones"] then
+  TECHNOLOGY("transport-system")
+    :add_prereq("logistics-2")
+    :add_prereq("advanced-electronics")
+
+  TECHNOLOGY("fast-road")
+    :add_prereq("concrete")
+
+
   for _, depot in pairs ({"request-depot", "supply-depot", "fuel-depot", "fluid-depot", "buffer-depot"}) do
     RECIPE(depot):add_ingredient({type = "item", name = "electronic-circuit", amount = 5})
   end
@@ -40,17 +102,18 @@ end
 
 
 -- Update AAI Ironclad
-if mods["miniloader"] then
+if mods["aai-vehicles-ironclad"] and mods["cargo-ships"] then
   TECHNOLOGY("ironclad")
     :remove_prereq("automobilism")
     :add_prereq("water_transport")
 end
 
 -- Update Hovercrafts
-if mods["Hovercrafts"] then
+if mods["Hovercrafts"] and mods["cargo-ships"] then
   TECHNOLOGY("hcraft-tech")
     :remove_prereq("automobilism")
     :add_prereq("robotics")
+    :add_prereq("water_transport")
 end
 
 -- Update Aircraft
@@ -80,3 +143,20 @@ end
 if mods["RampantArsenal"] then
   RECIPE("rifle-item-rampant-arsenal"):add_ingredient({type = "item", name = "motor", amount = 1})
 end
+
+-- data.raw.item["torch"]["electric_energy_source_prototype"]["buffer_capacity"] = "100kW"
+-- data.raw.item["torch"]["energy_usage"] = "37.8kW"
+-- data.raw.item["torch"]["energy_usage"] = "1TW"
+-- data.raw.item["torch"]["energy_consumption"] = "100MW"
+-- data.raw.item["torch"]["fuel_inventory_size"] = 0
+
+-- energy_usage = "37.8kW",
+-- energy_consumption = "1.8MW",
+-- fuel_inventory_size = 1,
+-- energy_source =
+--     {
+--       type = "electric",
+--       usage_priority = "secondary-output",
+--       buffer_capacity = "1TW",
+--       render_no_network_icon = false
+--     }
